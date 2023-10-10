b45b0823f6345260744fc02acf23a18dafa8a372
WilliamJohnsonKU <wiljohnson@ku.edu>
2023-10-10 18:28:50 -0500

    Testing functionality for supporting parens in the eval string

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <cmath>
 #include <string>
+#include <stack>
+#include <sstream>
 #include <cstring>
 #include <vector>
 //#include <iostream>
@@ -11,7 +13,7 @@ using namespace std;
     Should take in a simple equation like "a + b", "a - b", "a * b", etc. and return the solution
 */
 
-int performSimpleOperation(char* subEquationChars){
+int old_performSimpleOperation(char* subEquationChars){
     string subEquation(subEquationChars);
     vector<char> operands;
     for (char c : subEquation) {
@@ -48,6 +50,62 @@ int performSimpleOperation(char* subEquationChars){
     return result;
 }
 
+int performSimpleOperation(int a, int b, char op){
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        case '^': return pow(a, b);
+    }
+
+    return 0;
+}
+
+/* WARNING: CURRENT VERSION DOES NOT NECESSARILY FOLLOW PEMDAS */
+
+int evaluate(const char* s){
+    stack<int> numbers;
+    stack<char> ops;
+
+    for (int i = 0; i < s.length(); ++i){
+        char c = s[i];
+
+        if (isdigit(c)) {
+            int num = 0
+            while (isdigit(s[i])){
+                num = num * 10 + (s[i] - '0']);
+                i++;
+            }
+            i--;
+            numbers.push(num);
+        } else if (c == '('){
+            ops.push(c);
+        } else if (c == ')'){
+            while (!ops.empty() && ops.top() != '('){
+                int b = numbers.top();
+                numbers.pop();
+                int a = numbers.top();
+                numbers.pop();
+                numbers.push(operate(a, b, ops.top()));
+                ops.pop();
+            }
+            ops.push(c);
+        }
+    }
+
+    while (!ops.empty()){
+        int b = numbers.top();
+        numbers.pop();
+        int a = numbers.top();
+        numbers.pop();
+        numbers.push(operate(a, b, ops.top()));
+        ops.pop();
+    }
+
+    return numbers.top();
+}
+
 int main(int argc, char *argv[]){
     if (argc != 2){
         printf("Incorrect number of params (%d) for test code. Please provide a problem as a string", argc-1);
@@ -56,7 +114,7 @@ int main(int argc, char *argv[]){
 
     char* BaseEquation = argv[1];
     
-    performSimpleOperation(BaseEquation);
+    evaluate(BaseEquation);
 
     return 0;
 }
