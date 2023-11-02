2da33ab5f2db62b07e31194a9794df990779f740
WilliamJohnsonKU <wiljohnson@ku.edu>
2023-11-02 18:25:57 -0500

    Add paren support and PEMDAS order enforcement.

--- main.cpp
+++ main.cpp
@@ -13,44 +13,8 @@ using namespace std;
     Should take in a simple equation like "a + b", "a - b", "a * b", etc. and return the solution
 */
 
-int old_performSimpleOperation(char* subEquationChars){
-    string subEquation(subEquationChars);
-    vector<char> operands;
-    for (char c : subEquation) {
-        if (c == '+' || c == '-') {
-            operands.push_back(c);
-        }
-    }
-
-
-    char delimiters[] = "+-";
-    char* token = strtok(subEquationChars, delimiters);
-
-    int result = 0;
-    int itter = -1;
-
-    while (token != NULL){
-
-        if (itter < operands.size() && itter >= 0) {
-            if (operands[itter] == '+'){
-                result += atoi(token);
-            } else if (operands[itter] == '-'){
-                result -= atoi(token);
-            }
-        } else if (itter == -1) {
-            result = atoi(token);
-        }
-
-        token = strtok(NULL, delimiters);
-        itter++;
-    }
-    
-    printf("The result was %d\n", result);
-
-    return result;
-}
-
 int performSimpleOperation(int a, int b, char op){
+    printf("step: %d %c %d\n", a, op, b);
     switch (op) {
         case '+': return a + b;
         case '-': return a - b;
@@ -59,36 +23,57 @@ int performSimpleOperation(int a, int b, char op){
         case '^': return pow(a, b);
     }
 
+    printf("Unknown operator: %c", op);
+
     return 0;
 }
 
-/* WARNING: CURRENT VERSION DOES NOT NECESSARILY FOLLOW PEMDAS */
-
-int evaluate(const char* s){
+int evaluate(const string s){
     stack<int> numbers;
     stack<char> ops;
 
-    for (int i = 0; i < s.length(); ++i){
+    string s_cleaned_temp;
+
+    for (int i = 0; i < s.size(); ++i){
         char c = s[i];
 
+        if (isspace(c)) continue;
+
+        s_cleaned_temp += c;
+
         if (isdigit(c)) {
-            int num = 0
+            int num = 0;
             while (isdigit(s[i])){
-                num = num * 10 + (s[i] - '0']);
+                num = num * 10 + (s[i] - '0');
                 i++;
             }
             i--;
             numbers.push(num);
         } else if (c == '('){
+            if (i > 0 && isdigit(s_cleaned_temp[i - 1])){
+                ops.push('*');
+            }
             ops.push(c);
         } else if (c == ')'){
-            while (!ops.empty() && ops.top() != '('){
+            while (ops.top() != '(' && (!ops.empty())){
                 int b = numbers.top();
                 numbers.pop();
                 int a = numbers.top();
                 numbers.pop();
-                numbers.push(operate(a, b, ops.top()));
+                char op = ops.top();
                 ops.pop();
+                numbers.push(performSimpleOperation(a, b, op));
+            }
+            ops.pop();
+        } else {
+            while (!ops.empty() && ((c=='+') || (c=='-') || (c=='*') || (c=='/')) && (ops.top() == '*' || ops.top() == '/')){
+                int b = numbers.top();
+                numbers.pop();
+                int a = numbers.top();
+                numbers.pop();
+                char op = ops.top();
+                ops.pop();
+                numbers.push(performSimpleOperation(a, b, op));
             }
             ops.push(c);
         }
@@ -99,7 +84,7 @@ int evaluate(const char* s){
         numbers.pop();
         int a = numbers.top();
         numbers.pop();
-        numbers.push(operate(a, b, ops.top()));
+        numbers.push(performSimpleOperation(a, b, ops.top()));
         ops.pop();
     }
 
@@ -107,6 +92,7 @@ int evaluate(const char* s){
 }
 
 int main(int argc, char *argv[]){
+
     if (argc != 2){
         printf("Incorrect number of params (%d) for test code. Please provide a problem as a string", argc-1);
         return 0;
@@ -114,7 +100,9 @@ int main(int argc, char *argv[]){
 
     char* BaseEquation = argv[1];
     
-    evaluate(BaseEquation);
+    int result = evaluate(BaseEquation);
+
+    printf("\n\nResult: %d", result);
 
     return 0;
 }
